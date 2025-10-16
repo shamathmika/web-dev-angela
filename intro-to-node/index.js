8a83538484ce115bbe2118d2c75ccf8401291670
Shamathmika <vshamathmika@gmail.com>
2025-10-16 16:20:25 -0700

    uploading intro-to-node project

---
+++ intro-to-node/index.js
@@ -0,0 +1,11 @@
+//jshint esversion:6
+
+var superheroes = require("superheroes");
+var mySuperHero = superheroes.random();
+
+console.log("My super hero: " + mySuperHero);
+
+var supervillains = require("supervillains");
+var mySuperVillain = supervillains.random();
+
+console.log("My super villain: " + mySuperVillain);
