4393712406615c4addb5f4e6c80a81da71e8a46a
Shamathmika <vshamathmika@gmail.com>
2025-10-16 15:26:19 -0700

    Uploading Calculator project

---
+++ Calculator/calculator.js
@@ -0,0 +1,35 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+  res.send("Calculated: " + result);
+});
+
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+  var height = parseFloat(req.body.height);
+  var weight = parseFloat(req.body.weight);
+
+  var result = weight/(height * height);
+  res.send("BMI Calculated: " + result);
+});
+
+app.listen(3000, function(){
+  console.log("Started listening to port 3000");
+});
